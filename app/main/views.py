eac11fa206cc75d53decda48fd6acd4cf6bca069
eddy <eddyyonnie@gmail.com>
2019-05-02 18:35:37 +0300

    changes established

--- app/main/views.py
+++ app/main/views.py
@@ -12,3 +12,53 @@ def index():
     title = 'Home - Oa Online'
 
     return render_template('index.html', title = title)
+
+@main.route('/types')
+def types():
+   '''
+   View root page function that returns the types page and its data
+   '''
+
+   title = 'Types'
+
+   return render_template('types.html')
+
+@main.route('/importance')
+def importance():
+   '''
+   View root page function that returns the importance page and its data
+   '''
+
+   title = 'importance'
+
+   return render_template('importance.html')
+
+@main.route('/requirements')
+def requirements():
+   '''
+   View root page function that returns the requirements page and its data
+   '''
+
+   title = 'requirements'
+
+   return render_template('requirements.html')
+
+@main.route('/process')
+def process():
+   '''
+   View root page function that returns the process page and its data
+   '''
+
+   title = 'process'
+
+   return render_template('process.html')
+
+@main.route('/divorce')
+def divorce():
+   '''
+   View root page function that returns the divorce page and its data
+   '''
+
+   title = 'divorce'
+
+   return render_template('divorce.html')
