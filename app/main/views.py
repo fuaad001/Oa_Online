8d58a3214b70d00017ac274d95308bdda3dd80ba
fuaad001 <husseinfuaad@hotmail.com>
2019-05-02 18:28:23 +0300

    Create marriage models

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for, abort
 from . import main
 from .. import db, photos
 from flask_login import login_required, current_user
+from ..models import User, Notice, Certificate, Impediment
 
 @main.route('/')
 def index():
@@ -12,3 +13,260 @@ def index():
     title = 'Home - Oa Online'
 
     return render_template('index.html', title = title)
+
+@main.route('/types')
+def types():
+    '''
+    View root page function that returns the types page and its data
+    '''
+
+    title = 'Types'
+
+    return render_template('types.html')
+
+@main.route('/importance')
+def importance():
+    '''
+    View root page function that returns the importance page and its data
+    '''
+
+    title = 'importance'
+
+    return render_template('importance.html')
+
+@main.route('/requirements')
+def requirements():
+    '''
+    View root page function that returns the requirements page and its data
+    '''
+
+    title = 'requirements'
+
+    return render_template('requirements.html')
+
+@main.route('/process')
+def process():
+    '''
+    View root page function that returns the process page and its data
+    '''
+
+    title = 'process'
+
+    return render_template('process.html')
+
+@main.route('/divorce')
+def divorce():
+    '''
+    View root page function that returns the divorce page and its data
+    '''
+
+    title = 'divorce'
+
+    return render_template('divorce.html')
+
+@main.route('/divorce2')
+def divorce2():
+    '''
+    View root page function that returns the divorce2 page and its data
+    '''
+
+    title = 'divorce2'
+
+    return render_template('divorce2.html')
+
+@main.route('/process2')
+def process2():
+    '''
+    View root page function that returns the process2 page and its data
+    '''
+
+    title = 'process2'
+
+    return render_template('process2.html')
+
+@main.route('/laws')
+def laws():
+    '''
+    View root page function that returns the laws page and its data
+    '''
+
+    title = 'laws'
+
+    return render_template('laws.html')
+
+@main.route('/marry/<type>/notice', methods=["GET", "POST"])
+def notice(type):
+    '''
+    View root page function that returns the notice page and its data
+    '''
+
+    notice = Notice()
+
+    if request.method == "POST":
+		district = request.form["district"]
+        spouse = request.form["spouse"]
+        g_name = request.form["g_name"]
+        g_condition = request.form["g_condition"]
+        g_occupation = request.form["g_occupation"]
+        g_age = request.form["g_age"]
+        g_residence = request.form["g_residence"]
+        g_consent = request.form["g_consent"]
+        b_name = request.form["b_name"]
+        b_condition = request.form["b_condition"]
+        b_occupation = request.form["b_occupation"]
+        b_age = request.form["b_age"]
+        b_residence = request.form["b_residence"]
+        b_consent = request.form["b_consent"]
+        dd = request.form["dd"]
+        mm = request.form["mm"]
+        yy = request.form["yy"]
+        signature = request.form["signature"]
+
+        notice.district = district
+        notice.spouse = spouse
+        notice.g_name = g_name
+        notice.g_condition = g_condition
+        notice.g_occupation = g_occupation
+        notice.g_age = g_age
+        notice.g_residence = g_residence
+        notice.g_consent = g_consent
+        notice.b_name = b_name
+        notice.b_condition = b_condition
+        notice.b_occupation = b_occupation
+        notice.b_age = b_age
+        notice.b_residence = b_residence
+        notice.b_consent = b_consent
+        notice.dd = dd
+        notice.mm = mm
+        notice.yy = yy
+        notice.signature = signature
+        notice.user_id = current_user.id
+
+        db.session.add(notice)
+        db.session.commit()
+
+        return redirect(url_for('.index'))
+
+
+    title = 'Marriage Notice'
+
+    return render_template('marriages/notice.html')
+
+@main.route('/marry/sign_certificate')
+def certificate():
+    '''
+    View root page function that returns the certificate page and its data
+    '''
+
+    certificate = Certificate()
+
+    if request.method == "POST":
+        g_date = request.form["g_date"]
+        g_name = request.form["g_name"]
+        g_condition = request.form["g_condition"]
+        g_occupation = request.form["g_occupation"]
+        g_age = request.form["g_age"]
+        g_residence = request.form["g_residence"]
+        g_fname = request.form["g_fname"]
+        g_foccupation = request.form["g_foccupation"]
+        b_date = request.form["b_date"]
+        b_name = request.form["b_name"]
+        b_condition = request.form["b_condition"]
+        b_occupation = request.form["b_occupation"]
+        b_age = request.form["b_age"]
+        b_residence = request.form["b_residence"]
+        g_fname = request.form["b_fname"]
+        g_foccupation = request.form["b_foccupation"]
+        groom = request.form["groom"]
+        bride = request.form["bride"]
+        witness1 = request.form["witness1"]
+        witness2 = request.form["witness2"]
+        date = request.form["date"]
+
+        certificate.g_date = g_date
+        certificate.g_name = g_name
+        certificate.g_condition = g_condition
+        certificate.g_occupation = g_occupation
+        certificate.g_age = g_age
+        certificate.g_residence = g_residence
+        certificate.g_fname = g_fname
+        certificate.g_foccupation = g_foccupation
+        certificate.b_date = b_date
+        certificate.b_name = b_name
+        certificate.b_condition = b_condition
+        certificate.b_occupation = b_occupation
+        certificate.b_age = b_age
+        certificate.b_residence = b_residence
+        certificate.b_fname = b_fname
+        certificate.b_foccupation = b_foccupation
+        certificate.groom = groom
+        certificate.bride = bride
+        certificate.witness1 = witness1
+        certificate.witness2 = witness2
+        certificate.date = date
+        certificate.user_id = current_user.id
+
+
+        db.session.add(certificate)
+        db.session.commit()
+
+        return redirect(url_for('.index'))
+
+    title = 'Marriage Certificate'
+
+    return render_template('marriages/certificate.html')
+
+@main.route('/marry/impediment_certificate')
+def impediment():
+    '''
+    View root page function that returns the impediment page and its data
+    '''
+
+    impediment = Impediment()
+
+    if request.method == "POST":
+        spouse = request.form["spouse"]
+        at = request.form["at"]
+        in_input = request.form["in_input"]
+        surname = request.form["surname"]
+        forename = request.form["forename"]
+        country = request.form["country"]
+        date = request.form["date"]
+        father = request.form["father"]
+        sex = request.form["sex"]
+        race = request.form["race"]
+        religion = request.form["religion"]
+        residence = request.form["residence"]
+        condition = request.form["condition"]
+        occupation = request.form["occupation"]
+        dd = request.form["dd"]
+        mm = request.form["mm"]
+        yy = request.form["yy"]
+        signature = request.form["signature"]
+
+        impediment.spouse = spouse
+        impediment.at = at
+        impediment.in_input = in_input
+        impediment.surname = surname
+        impediment.forename = forename
+        impediment.country = country
+        impediment.date = date
+        impediment.father = father
+        impediment.sex = sex
+        impediment.race = race
+        impediment.religion = religion
+        impediment.residence = residence
+        impediment.occupation =  occupation
+        impediment.condition = condition
+        impediment.dd = dd
+        impediment.mm = mm
+        impediment.yy = yy
+        impediment.signature = signature
+        impediment.user_id = current_user.id
+
+        db.session.add(impediment)
+        db.session.commit()
+
+        return redirect(url_for('.index'))
+
+    return render_template('marriages/impediment.html')
--- app/models.py
+++ app/models.py
@@ -22,6 +22,9 @@ class User(UserMixin, db.Model):
     password_hash = db.Column(db.String(255))
     husband_pic_path = db.Column(db.String(255))
     wife_pic_path = db.Column(db.String(255))
+    notices = db.relationship("Notice", backref= "user", lazy="dynamic")
+    certificates = db.relationship("Certificate", backref= "user", lazy="dynamic")
+    impediments = db.relationship("Impediment", backref= "user", lazy="dynamic")
 
     @property
     def password(self):
@@ -36,3 +39,76 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f'User {self.husband_name} and {self.wife_name}'
+
+class Notice(db.Model):
+    __tablename__ = 'notices'
+
+    id = db.Column(db.Integer, primary_key = True)
+    district = db.Column(db.String(255))
+    spouse = db.Column(db.String(255))
+    g_name = db.Column(db.String(255))
+    g_condition = db.Column(db.String(255))
+    g_occupation = db.Column(db.String(255))
+    g_age = db.Column(db.Integer)
+    g_residence = db.Column(db.String(255))
+    g_consent = db.Column(db.String(255))
+    b_name = db.Column(db.String(255))
+    b_condition = db.Column(db.String(255))
+    b_occupation = db.Column(db.String(255))
+    b_age = db.Column(db.Integer)
+    b_residence = db.Column(db.String(255))
+    b_consent = db.Column(db.String(255))
+    dd = db.Column(db.Integer)
+    mm = db.Column(db.String(255))
+    yy = db.Column(db.Integer)
+    signature = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+class Certificate(db.Model):
+    __tablename__ = 'certificates'
+
+    g_date = db.Column(db.Date())
+    g_name = db.Column(db.String(255))
+    g_condition = db.Column(db.String(255))
+    g_occupation = db.Column(db.String(255))
+    g_age = db.Column(db.Integer)
+    g_residence = db.Column(db.String(255))
+    g_fname = db.Column(db.String(255))
+    g_foccupation = db.Column(db.String(255))
+    b_date = db.Column(db.Date())
+    b_name = db.Column(db.String(255))
+    b_condition = db.Column(db.String(255))
+    b_occupation = db.Column(db.String(255))
+    b_age = db.Column(db.Integer)
+    b_residence = db.Column(db.String(255))
+    g_fname = db.Column(db.String(255))
+    g_foccupation = db.Column(db.String(255))
+    groom = db.Column(db.String(255))
+    bride = db.Column(db.String(255))
+    witness1 = db.Column(db.String(255))
+    witness2 = db.Column(db.String(255))
+    date = db.Column(db.Date())
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+
+class Impediment(db.Model):
+    __tablename__ = 'impediments'
+
+    spouse = db.Column(db.String(255))
+    at = db.Column(db.String(255))
+    in_input = db.Column(db.String(255))
+    surname = db.Column(db.String(255))
+    forename = db.Column(db.String(255))
+    country = db.Column(db.String(255))
+    date = db.Column(db.Date())
+    father = db.Column(db.String(255))
+    sex = db.Column(db.String(255))
+    race = db.Column(db.String(255))
+    religion = db.Column(db.String(255))
+    residence = db.Column(db.String(255))
+    condition = db.Column(db.String(255))
+    occupation = db.Column(db.String(255))
+    dd = db.Column(db.Integer)
+    mm = db.Column(db.String(255))
+    yy = db.Column(db.Integer)
+    signature = db.Column(db.String(255))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
